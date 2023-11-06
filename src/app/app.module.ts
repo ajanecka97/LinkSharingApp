8d359cbcefcb72461f6fe05605bbf214bd80c385
Aleksandra Janecka <ajanecka97@gmail.com>
2023-11-06 06:54:15 +0100

    feat(login): add styles to auth box component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { AuthBoxComponent } from './auth-box/auth-box.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AuthBoxComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
