7adf5a66e5d37fa666e4667d4a3136aef2a381fb
Aleksandra Janecka <ajanecka97@gmail.com>
2023-11-14 07:51:59 +0100

    refactor: remove routing module

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,10 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
   {
     path: 'sign-in',
@@ -27,9 +26,3 @@ const routes: Routes = [
     canActivate: [authGuard],
   },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app/app.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/routes';
 
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, ReactiveFormsModule, AngularSvgIconModule.forRoot()),
-        provideHttpClient(withInterceptorsFromDi())
+        importProvidersFrom(BrowserModule, ReactiveFormsModule, AngularSvgIconModule.forRoot()),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideRouter(routes)
     ]
 })
   .catch(err => console.error(err));
