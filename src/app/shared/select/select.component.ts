688d5e607e61fe3e5af4409fb864e75e0d967a49
Aleksandra Janecka <ajanecka97@gmail.com>
2023-11-20 06:35:17 +0100

    feat(select): more attempts on select box

--- src/app/shared/select/select.component.ts
+++ src/app/shared/select/select.component.ts
@@ -1,12 +1,16 @@
 import {
+  AfterViewInit,
   Component,
+  ElementRef,
   EventEmitter,
   Input,
+  OnDestroy,
   OnInit,
   Output,
   Renderer2,
   TemplateRef,
   ViewChild,
+  signal,
 } from '@angular/core';
 import { CommonModule, NgFor } from '@angular/common';
 import { SelectOption } from './select.model';
@@ -21,20 +25,45 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.scss'],
 })
-export class SelectComponent<T> {
+export class SelectComponent<T> implements AfterViewInit, OnDestroy {
   @Input() public inputId!: string;
   @Input() public options!: SelectOption<T>[];
   @Input() public label!: string;
 
   @Output() public selectedOptionChanged = new EventEmitter<T>();
 
+  @ViewChild('selectInput') public selectInput!: ElementRef;
+  @ViewChild('selectBox') public selectBox!: ElementRef;
+
   public inputFocused$: Observable<boolean>;
   public selectedOption: SelectOption<T> | null = null;
-
   public inputFocusedSubject$ = new BehaviorSubject(false);
+  public selectBoxOpened = signal(false);
 
-  constructor() {
+  private outsideClickFunction: () => void = () => {};
+
+  constructor(private renderer: Renderer2) {
     this.inputFocused$ = this.inputFocusedSubject$.pipe(shareReplay(2));
+  }
+
+  public ngAfterViewInit(): void {
+    this.outsideClickFunction = this.renderer.listen(
+      'window',
+      'click',
+      (e: Event) => {
+        console.log(e.target, this.selectInput, this.selectBox);
+        if (
+          e.target !== this.selectInput.nativeElement &&
+          e.target !== this.selectBox?.nativeElement
+        ) {
+          this.inputFocusedSubject$.next(false);
+        }
+      }
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.outsideClickFunction();
   }
 
   public selectOption(option: SelectOption<T>) {
